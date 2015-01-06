60445d5f45a227b484b025f5ea072d61baea1a1c
Tomasz Duda <tomek@ddart.pl>
2015-01-06 21:10:20 +0000

    Added the MongoDB v2.6.6

--- bootstrap.sh
+++ bootstrap.sh
@@ -174,6 +174,15 @@ sudo bash -c "echo '${vhost}' > /etc/apache2/sites-available/000-default.conf"
 sudo bash -c "curl -sS https://getcomposer.org/installer | php"
 sudo mv composer.phar /usr/local/bin/composer
 
+
+# MONGODB ##############################################################################################################
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' \
+ | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+
 # RESTART ##############################################################################################################
 sudo service apache2 restart
 sudo service php5-fpm restart
